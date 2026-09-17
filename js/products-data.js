5276c0b13c337d6e5fad39b86f05f8cf4bfbed6c
navidedalaty91-code <navidedalaty91@gmail.com>
2026-09-18 01:54:15 +0330

    Update products-data.js

--- js/products-data.js
+++ js/products-data.js
@@ -3,6 +3,7 @@ const products = {
         name: "هواپیما کنترلی",
         price: "۳,۰۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-35.jpg",
+        description:"پروازی هیجان‌انگیز و سرگرم‌کننده ✈️",
         link: "../product7/"
     },
 
@@ -10,6 +11,7 @@ const products = {
         name: "بوکسینگ شارژی بلوتوث",
         price: "۵,۱۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-33.jpg",
+        description:"مبارزه هیجان‌انگیز با بلوتوث 🥊",
         link: "../product5/"
     },
 
@@ -17,6 +19,7 @@ const products = {
         name: "تفنگ ۲ کاره توپی و ابپاش",
         price: "۲,۲۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-25.jpg",
+        description:"دو بازی در یک تفنگ جذاب 🎯",
         link: "../product4/"
     },
 
@@ -24,30 +27,36 @@ const products = {
         name: "ست نجاری دلر باتری خور",
         price: "۲,۷۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-22.jpg",
+        description:"ست ابزار مهندسی جذاب و سرگرم‌کننده🔨",
         link: "../product3/"
     },
     Game_console_with_500_games: {
         name: "کنسول بازی ۵۰۰ بازی",
         price: "۲,۸۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-28.jpg",
+        description:"گیم‌پد شارژی با ۵۰۰ بازی متنوع 🎮",
         link: "../product/"
     },
     Hair_braiding_set: {
         name: "ست بافت مو",
         price: "۱,۳۰۰,۰۰۰ تومان",
         image: "../../images/Untitled-1.png",
+        description:"ست جذاب برای بافت مو 🎀",
         link: "../product1/"
     },
     Nail_and_Makeup_Set: {
         name: "ست ناخن و ارایشی",
         price: "۲,۰۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-19.jpg",
+        description:"ست جذاب ناخن 💅",
         link: "../product2/"
     },
     Instant_print_camera: {
         name: "دوربین عکاسی چاپ گردار",
         price: "۴,۵۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-30.jpg",
+        description: "لحظه‌ها را ثبت و چاپ کن📸",
         link: "../product6/"
     }
 };
+export { products };
