42346d62f74348b1bf75343336d07d63e583c56b
navidedalaty91-code <navidedalaty91@gmail.com>
2026-09-17 13:25:43 +0330

    Update related-products2.js

--- js/related-products.js
+++ js/related-products.js
@@ -1,12 +1,12 @@
 const popularProducts = [
     "Remote_controlled_airplane",
+    "Instant_print_camera",
     "Rechargeable_Bluetooth_Boxing_Machine",
     "Dual_function_ball_water_gun",
     "Carpentry_set_with_battery_powered_drill",
     "Game_console_with_500_games",
     "Hair_braiding_set",
-    "Nail_and_Makeup_Set",
-    "Instant_print_camera"
+    "Nail_and_Makeup_Set"
 ];
 
 const relatedContainer = document.querySelector(".related-products");
