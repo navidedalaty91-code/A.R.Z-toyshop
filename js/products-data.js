1793f8f481b5c27378a9b84101b2ceb425229a80
navidedalaty91-code <navidedalaty91@gmail.com>
2026-09-18 15:43:24 +0330

    Update products-data.js

--- js/products-data.js
+++ js/products-data.js
@@ -3,60 +3,73 @@ const products = {
         name: "هواپیما کنترلی",
         price: "۳,۰۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-35.jpg",
-        description:"پروازی هیجان‌انگیز و سرگرم‌کننده ✈️",
-        link: "../product7/"
+        description: "پروازی هیجان‌انگیز و سرگرم‌کننده ✈️",
+        link: "../../products/product7/",
+        category: "remote"
     },
 
     Rechargeable_Bluetooth_Boxing_Machine: {
         name: "بوکسینگ شارژی بلوتوث",
         price: "۵,۱۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-33.jpg",
-        description:"مبارزه هیجان‌انگیز با بلوتوث 🥊",
-        link: "../product5/"
+        description: "مبارزه هیجان‌انگیز با بلوتوث 🥊",
+        link: "../../products/product5/",
+        category: "action"
     },
 
     Dual_function_ball_water_gun: {
         name: "تفنگ ۲ کاره توپی و ابپاش",
         price: "۲,۲۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-25.jpg",
-        description:"دو بازی در یک تفنگ جذاب 🎯",
-        link: "../product4/"
+        description: "دو بازی در یک تفنگ جذاب 🎯",
+        link: "../../products/product4/",
+        category: "action"
     },
 
     Carpentry_set_with_battery_powered_drill: {
         name: "ست نجاری دلر باتری خور",
         price: "۲,۷۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-22.jpg",
-        description:"ست ابزار مهندسی جذاب و سرگرم‌کننده🔨",
-        link: "../product3/"
+        description: "ست ابزار جذاب و سرگرم‌کننده 🔨",
+        link: "../../products/product3/",
+        category: "tools"
     },
+
     Game_console_with_500_games: {
         name: "کنسول بازی ۵۰۰ بازی",
         price: "۲,۸۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-28.jpg",
-        description:"گیم‌پد شارژی با ۵۰۰ بازی متنوع 🎮",
-        link: "../product/"
+        description: "گیم‌پد شارژی با ۵۰۰ بازی متنوع 🎮",
+        link: "../../products/product/",
+        category: "console"
     },
+
     Hair_braiding_set: {
         name: "ست بافت مو",
         price: "۱,۳۰۰,۰۰۰ تومان",
         image: "../../images/Untitled-1.png",
-        description:"ست جذاب برای بافت مو 🎀",
-        link: "../product1/"
+        description: "ست جذاب برای بافت مو 🎀",
+        link: "../../products/product1/",
+        category: "makeup"
     },
+
     Nail_and_Makeup_Set: {
         name: "ست ناخن و ارایشی",
         price: "۲,۰۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-19.jpg",
-        description:"ست جذاب ناخن 💅",
-        link: "../product2/"
+        description: "ست جذاب ناخن 💅",
+        link: "../../products/product2/",
+        category: "makeup"
     },
+
     Instant_print_camera: {
         name: "دوربین عکاسی چاپ گردار",
         price: "۴,۵۰۰,۰۰۰ تومان",
         image: "../../images/photo_2026-09-13_22-49-30.jpg",
-        description: "لحظه‌ها را ثبت و چاپ کن📸",
-        link: "../product6/"
+        description: "لحظه‌ها را ثبت و چاپ کن 📸",
+        link: "../../products/product6/",
+        category: "digital"
     }
 };
+
 export { products };
